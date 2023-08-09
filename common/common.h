52a0e696d3e0f44e3246e8c4f4a90659805cde7d
jnomikos <john.nomikos55@gmail.com>
2023-08-09 15:50:26 -0500

    Generated MAV_CMD_DO_SET_EMERGENCY changes

--- common/common.h
+++ common/common.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_XML_HASH -6147130858909430823
+#define MAVLINK_COMMON_XML_HASH 3663483564331622384
 
 #ifdef __cplusplus
 extern "C" {
